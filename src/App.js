b8ea5136cf450f25cc9cd48a9a9ba0e8baf545b9
서예원 <seoyaewon0724@gmail.com>
2026-07-14 08:20:42 +0000

    feat : 틱택토 게임 완성( 플레이어 구분 추가 및 승자 결정 )

--- src/App.js
+++ src/App.js
@@ -9,17 +9,68 @@ function Square({value,onSquareClick}) {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default function Board() {
+
+  //다음 플레이어가 누구인지 판단하기 위한 상태값
+  const [xIsNext,setXIsNext] = useState(true);
+
   const [squares,setSquares] = useState(Array(9).fill(null));
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i){
+    //만약에 기존에 사각형에 값이 먼저 채워져 있다면 그냥 종료
+    //혹시 승자가 존재한다면 종료
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-     nextSquares[i] ='X';
+    
+    //플레이어가 X 일 때
+    if(xIsNext){
+      nextSquares[i] ="X";
+    }
+    //플레이어가 O 일 때
+    else{
+      nextSquares[i] ='O';
+    }
+    
      setSquares(nextSquares);
+
+     //한번 실행했으면 다시 실행될 때는 플레이어가 바뀌어져 있어야 함
+     setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
